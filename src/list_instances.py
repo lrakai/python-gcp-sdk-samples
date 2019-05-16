284774b6079b6e18acea727be5c123978352a1f5
Logan Rakai <lmrakai@gmail.com>
2019-05-16 16:18:02 -0600

    Refactor out credential code

--- src/list_instances.py
+++ src/list_instances.py
@@ -4,19 +4,13 @@ import time
 from config import EVENT_CONFIG
 
 from googleapiclient.discovery import build
-from google.oauth2.service_account import Credentials
+from credentials import credential_helper
 
 def handler(event, context):
-    credentials = get_credentials(event)
+    credentials = credential_helper.get_credentials(event)
     service = build('compute', 'v1', credentials=credentials)
     result = service.instances().list(project=credentials.project_id, zone='us-central1-a').execute()
     return True if 'items' in result else False
-
-def get_credentials(event):
-    service_account_info = json.loads(event['credentials']['credential_key'])
-    credentials = Credentials.from_service_account_info(service_account_info)
-    credentials = credentials.with_scopes(['https://www.googleapis.com/auth/cloud-platform'])
-    return credentials
 
 def timed_handler(event, context):
     start = time.time()
